713e4bb6641daa0c17b728137f0951e03ba8ef09
PSNAppZ <thepsnarayanan@gmail.com>
2024-06-06 19:08:11 +0530

    Add BaseMiddleware class

--- openg2p-fastapi-common/src/openg2p_fastapi_common/middleware.py
+++ openg2p-fastapi-common/src/openg2p_fastapi_common/middleware.py
@@ -11,7 +11,6 @@ _config = Settings.get_config(strict=False)
 class BaseMiddleware(BaseComponent):
     def __init__(self, name="", **kwargs):
         super().__init__(name=name)
-        self.middleware = None
 
     def post_init(self):
         app_registry.get().add_middleware(self.__class__)
